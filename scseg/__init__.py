6e8eedba46a5e68b2cb2dc58386f901f67e10f47
duanhongyi <devnull@localhost>
2012-11-05 08:37:23 +0800

    删除keywords算法

--- scseg/__init__.py
+++ scseg/__init__.py
@@ -9,11 +9,3 @@ def seg_text(text):
         encoding = chardet.detect(text)['encoding']
         text = unicode(text,encoding)
     return [word for word in Splitter(text)]
-
-def keywords(text):
-    #制作关键词,联合单个字
-    if not isinstance(text,unicode):
-        encoding = chardet.detect(text)['encoding']
-        text = unicode(text,encoding)
-    words = Keyword(text)
-    return set([word for word in words])
