0d32e2500dd06b167a94a7fcd7f29ab599c34fdc
duanhongyi <devnull@localhost>
2012-12-04 21:14:33 +0800

    修改words

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
     ]
 
 setup(name='scseg',
-      version='1.6.1',
+      version='1.6.12',
       description='a python chinese seg word',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
