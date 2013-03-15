832029b8156c0bc2283ca3ae8cdee86ea3efb2c6
duanhongyi <devnull@localhost>
2013-03-15 16:04:51 +0800

    修改版本号

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
     ]
 
 setup(name='scseg',
-      version='1.7.1',
+      version='1.7.2',
       description='a python chinese seg word',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
